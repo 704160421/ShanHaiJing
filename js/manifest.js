32eeb199ab176655dfebd9a896437a86053ef5a9
704160421 <704160421@qq.com>
2025-08-15 08:57:56 +0800

    Add files via upload

---
+++ js/manifest.js
@@ -0,0 +1,16 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '6602293dfa595bcd33012f02';
+_mrmcp['creative_id'] = '6758f94baa12b7a825050bfe';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 320;
+_mrmcp['height'] = _mrmcp['height'] || 626;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = '山海异兽';
+_mrmcp['build_number'] = parseInt('427');
+_mrmcp['publish_time'] = 'Sun Jun 15 2025 14:43:50 GMT+0800 (China Standard Time)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+_mrmcp['data_server'] = "https://weika.mugeda.cn";
+})();
